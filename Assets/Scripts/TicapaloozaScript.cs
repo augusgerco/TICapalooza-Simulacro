6d38c544e389d79a6485f33815def764666fd600
augusgerco <augustosgercovich@gmail.com>
2025-04-22 08:45:09 -0300

    jidsaj

---
+++ Assets/Scripts/TicapaloozaScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicapaloozaScript : MonoBehaviour
+{
+    public string entradasCampo;
+    public string entradasPlateas;
+
+    string valorCampo = 1200;
+    string valorPlateas = 2000;
+    string totalEntradasVendidas = entradasCampo + entradasPlateas;
+    string capacidadPredio = 36600;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (totalEntradasVendidas == capacidadPredio) {
+            Debug.Log("Sold out!");
+            return
+        }
+        if (totalEntradasVendidas >= capacidadPredio/2) {
+            Debug.Log("El festival fue un éxito!");
+            return
+        } else {
+            Debug.Log("Debemos mejorar la convocatoria");
+            return
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
